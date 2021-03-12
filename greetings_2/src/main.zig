ef8f6001b1edc45e03fcf95ce86e4c0998384ffb
shoshin <shoshinprograms@gmail.com>
2021-03-12 12:45:17 +1100

    Fix Greetings bug

--- greetings_2/src/main.zig
+++ greetings_2/src/main.zig
@@ -11,7 +11,8 @@ pub fn main() anyerror!void {
     var inputBuf: [4096]u8 = undefined;
 
     try stdout.print(msg, .{});
-    const name = try stdin.read(&inputBuf);
+    const cnt = try stdin.read(&inputBuf);
     // TODO: fix printing words that are uninitialised
-    try stdout.print("Hi, {}!\n", .{inputBuf});
+    const line = std.mem.trimRight(u8, inputBuf[0..cnt], "\n");
+    try stdout.print("Hi, {}!\n", .{line});
 }
